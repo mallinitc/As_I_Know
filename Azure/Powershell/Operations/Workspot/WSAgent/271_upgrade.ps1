63011d098b35cd40c3152a8e8b128f92c887488c
Mallikarjuna Reddy <malli.nitc@gmail.com>
2023-12-24 21:28:10 +0530

    Add files via upload

---
+++ Azure/Powershell/Operations/Workspot/WSAgent/271_upgrade.ps1
@@ -0,0 +1,4 @@
+﻿Stop-Service -name *workspot*
+Stop-process -name *workspot*
+taskkill /IM msiexec.exe /f
+msiexec.exe /i c:\programdata\workspotmsiupgrade\setup.msi /q /LOG "upgrade1.log"
---
+++ Azure/Powershell/Operations/Workspot/WSAgent/Agent_Reinstall_New.ps1
@@ -0,0 +1,30 @@
+﻿$Ws = Import-Excel -Path D:\Inputfile.xlsx|Out-GridView -PassThru
+
+$Vms = gc "D:\2.7.1_upgrade\2.7.1_upgrade\vms.txt"
+$Vms.count
+
+$SubscriptionID = '##'
+$TenantId = '##'
+
+Clear-AzContext -Scope CurrentUser -Force
+Connect-AzAccount -Subscription $SubscriptionID -Tenant $TenantId
+
+Foreach($Vm in $Vms)
+ {
+    
+            $Vmname= $VM
+            $Vmname= $Vmname.trim()
+            write-host $VMname
+            $AzVm = Get-AzVM -Name $Vmname
+            Write-Host $AzVm
+            $Out=Invoke-AzVMRunCommand  -ResourceGroupName $AzVm.ResourceGroupName -VMName $Vmname -CommandId RunPowerShellScript -ScriptPath "D:\2.7.1_upgrade\2.7.1_upgrade\271_upgrade.ps1" -AsJob
+            write-host $Out
+ }
+
+ while (Get-Job -State "Running")
+{
+    Write-Host "Jobs are still running..."
+    Start-Sleep -s 5
+}
+Get-Job -State Completed|Remove-Job
+Get-Job -State Failed|Remove-Job
