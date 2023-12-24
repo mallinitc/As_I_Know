13412e249ba6e5669d26ac94456758e77f56d94d
Mallikarjuna Reddy <malli.nitc@gmail.com>
2023-12-24 21:35:21 +0530

    Add files via upload

---
+++ Azure/Powershell/Reports/Workspot/Get-AzVM_Summary.ps1
@@ -0,0 +1,27 @@
+﻿$Ws = Import-Excel -Path C:\H_Skus\AzureCustomers-April2020.xlsx -WorksheetName 'USA CSP'|Out-GridView -PassThru
+$Excel = "C:\H_Skus\Output.xlsx"
+ForEach($Cust in $Ws)
+{
+    $Name=$Cust.Customer
+    Clear-AzContext -Scope CurrentUser -Force
+    Connect-AzAccount -Subscription $Cust.SubscriptionId -Tenant $Cust.TenantId
+    $AllVms = Get-AzVM
+    [System.Collections.ArrayList] $MainObj = @()
+    ForEach ($Vm in $AllVms)
+    {
+        $Size=($Vm|select -ExpandProperty  HardwareProfile).VmSize
+        #$OS=($Vm|select -ExpandProperty  OSProfile).OsType
+        $obj = New-Object PsObject -Property @{
+        Customer = $Cust.Customer
+        VMName = $Vm.Name
+        RGName = $vm.ResourceGroupName
+        SKU = $Size
+        Location = $vm.Location
+        OSType = $vm.LicenseType
+        }
+        $MainObj.Add($obj) | Out-Null
+
+    }
+    $MainObj|Select-Object Customer,VMName,RGName,SKU,Location,OSType | Export-Excel -WorksheetName $Name -Path $Excel -Append
+    
+}
---
+++ Azure/Powershell/Reports/Workspot/Get_VMs_with_Skus.ps1
@@ -0,0 +1,69 @@
+﻿$Skus = @{Standard_NV6='Standard_NV6h';
+      Standard_NV6_Promo='Standard_NV6h';
+      Standard_D2s_v3='Standard_D2hs_v3';
+      Standard_NV4as_v4='Standard_NV4ahs_v4';
+      Standard_D4s_v3='Standard_D4hs_v3';
+      Standard_NV12s_v3='Standard_NV12hs_v3'
+    }
+
+
+$Excel = "C:\H_Skus\" + "Output.xlsx"
+
+$AllPools = Import-Excel -Path C:\H_Skus\Book4_VM.xlsx -WorksheetName 'USA'
+$CustData = Import-Excel -Path 'C:\H_Skus\AzureCustomers-April2020.xlsx'
+
+$Customers = $AllPools|Select 'Customer Name' -Unique
+
+
+Foreach($Customer in $Customers.'Customer Name')
+{
+    $Customer = $Customer.ToString()
+    [System.Collections.ArrayList] $Output=@()
+    $Custpools = $AllPools |Where-Object {(($_.'Customer Name' -like $Customer) -and ($_.'Pool Type' -like 'Persistent'))}
+    If(!($Custpools))
+    {
+        Continue
+    }
+    $Subscription = ($CustPools|Select Subscription -First 1).subscription
+    $TenantId=($CustData|Where-Object {$_.SubscriptionId -like $Subscription}).TenantId
+    Clear-AzContext -Force -Scope CurrentUser
+    If(!(Connect-AzAccount -Subscription $Subscription -Tenant $TenantId))
+    {
+        Continue
+    }
+    $AllVMs = Get-AzVM
+    Foreach($CustPool in $Custpools)
+    {
+        #VMs that matches Given DesktopName
+        foreach($Sku in $Skus.Keys)
+        {
+            #Find VMs
+            $SkusVMs=$AllVMs|Where-Object {($_.HardwareProfile.VmSize -like $Sku) -and ($_.location -notlike 'australiaeast')}
+
+            foreach($Skuvm in $SkusVMs)
+            {
+                #Find if VM starts with Desktopname
+                $SearchString=$CustPool.'Desktop Name'+'*'
+                If($Skuvm.Name -like $SearchString)
+                {
+                    $TargetSku=$Skus[$Sku]
+                    #Change the SKU here
+                    $obj = New-Object PsObject -Property @{
+                    Customer = $Customer
+                    VMName = $Skuvm.Name
+                    SKU = $Sku
+                    TargetSku= $TargetSku
+                    Subscription=$Subscription
+                    Tenant=$TenantId
+                    }
+                    $Output.Add($obj) | Out-Null
+                }
+            }
+        }
+    }
+    $Output | Select-Object Customer,VMName,Sku,TargetSku,Subscription,Tenant| Export-Excel -Path $Excel -Append -WorksheetName $Customer
+    
+}
+
+
+
