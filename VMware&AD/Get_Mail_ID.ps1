ad2678c0dbf04f2717b493c5a3b0c8df34718948
Mallikarjuna Reddy <malli.nitc@gmail.com>
2023-12-28 23:42:06 +0530

    Add files via upload

---
+++ VMware&AD/Get_Mail_ID.ps1
@@ -0,0 +1,20 @@
+﻿##Streamed / AD group
+
+$users=(Get-ADGroupMember NAME).SamAccountName
+foreach($user in $users)
+{
+    $usr=$user.Trim()
+
+    (Get-ADUser $usr -Properties *).EmailAddress
+}
+
+
+##StreamedD
+
+$users=(Get-BrokerDesktop -AdminAddress NAME -CatalogName "NAME").AssociatedUserNames
+foreach($user in $users)
+{
+    $usr=$user.Trim()
+    $usr=$usr.Replace("VDSI\","")
+    (Get-ADUser $usr -Properties *).EmailAddress
+}
